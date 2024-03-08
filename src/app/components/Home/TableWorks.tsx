1aca3c7d04bb55abce352b85397fc36ed699b41c
NahuelNGomez <nngomez@fi.uba.ar>
2024-03-08 12:04:31 -0300

    new project added

--- src/app/components/Home/TableWorks.tsx
+++ src/app/components/Home/TableWorks.tsx
@@ -33,7 +33,7 @@ export default function TableWorks({ image, title, description, link, tags }: co
       
       <div className="flex justify-center items-center pt-4">
         <Link href={link} target="_blank">
-          <button type="button" className=" text-white bg-dark0 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-2xl text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-dark0 dark:hover:bg-gray-900 dark:focus:ring-gray-900 dark:border-gray-900">
+          <button type="button" className=" text-white bg-dark0 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-2xl text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-dark0 dark:hover:bg-gray-900  transition duration-200 dark:focus:ring-gray-900 dark:border-gray-900">
             <section className="flex justify-center items-center gap-2">
               <svg className="h-6 w-6 text-white " viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">  <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" /></svg>
               view Project
