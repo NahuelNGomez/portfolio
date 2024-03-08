a0c8e8c923936f5792d3b2f4318cd4bd6878959f
NahuelNGomez <nngomez@fi.uba.ar>
2024-03-07 21:20:19 -0300

    tags added to the projects & constants module

--- src/app/components/Home/TableWorks.tsx
+++ src/app/components/Home/TableWorks.tsx
@@ -6,29 +6,40 @@ interface componentsArgs {
   title: string;
   description: string;
   link: string;
+  tags: any;
 }
 
-export default function TableWorks({ image, title, description, link }: componentsArgs) {
+export default function TableWorks({ image, title, description, link, tags }: componentsArgs) {
   return (
-
     <div className="bg-white flex-col min-w-min min-h-48 items-center justify-center p-4 rounded-lg shadow-md">
       <div className="bg-black flex-col sm:w-full h-52 md:h-72 overflow-hidden rounded-lg relative items-center justify-center">
         <Image
           src={image}
           alt=""
           layout="fill"
-          width={4000}
-          height={3000}
           className="rounded-lg object-cover"
         />
       </div>
 
       <h3 className="text-xl max-h-24 text-black font-semibold text-center p-2">{title}</h3>
+      <section className="flex flex-wrap justify-center sm:justify-center items-center gap-2">
+            {
+              tags ? tags.map((tag: any) => {
+                return <div key={tag.name} className={tag.class}>{tag.icon}<div className="sm:flex hidden">{tag.name}</div></div>
+              }) : ""
+            }
+          </section>
       <p className="text-gray-600 text-center">{description}</p>
+      
       <div className="flex justify-center items-center pt-4">
         <Link href={link} target="_blank">
-          <button type="button" className=" text-white bg-dark0 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-dark0 dark:hover:bg-gray-900 dark:focus:ring-gray-900 dark:border-gray-900">view Project
+          <button type="button" className=" text-white bg-dark0 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-2xl text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-dark0 dark:hover:bg-gray-900 dark:focus:ring-gray-900 dark:border-gray-900">
+            <section className="flex justify-center items-center gap-2">
+              <svg className="h-6 w-6 text-white " viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">  <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" /></svg>
+              view Project
+            </section>
           </button>
+          
         </Link>
       </div>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,59 +6,22 @@ import Navegation from './components/Home/Navegation'
 import About from './components/Home/About';
 import TableWorks from './components/Home/TableWorks';
 import ContactMe from './components/Home/ContactMe';
+import { TAGS, PROJECTS } from './constants/constants';
 
 export default function Home() {
-
-  const rocketLeague = "https://i.ibb.co/brRpmFm/rocket-League.jpg";
-  const teg = "https://i.ibb.co/7vtvyYT/Teg.png";
-  const kotlin = "https://i.ibb.co/nz88r5Z/kotlin.png";
-  const frontPSA = "https://i.ibb.co/DYLtyXF/Front-Soporte.png"
-  const apiRest = "https://i.ibb.co/2Z5hjqS/rest.png"
-  const fileTransfer = "https://i.ibb.co/sQ8Vwf9/file-transfer.jpg"
-
-  const titleRocketLeague = "Rocket League 2D"
-  const titleTeg = "T - E - G"
-  const titleKotlin = "Comparador APP - Kotlin"
-  const titlefrontPSA = "Frontend PSA"
-  const titleApiRest = "API REST"
-  const titleFileTransfer = "File Transfer"
-
-  const descripcionRocketLeague = "Trabajo realizado en C++ con conceptos de Cliente - Servidor, Sockets TCP, Multithreading y herramientas de visualización como QT5 y SDL2. Proyecto compartido."
-  const descripcionTeg = "Proyecto realizado en Java utilzando POO, TDD y diagramas UML. Se utiliza como herramienta gráfica a JavaFX. Proyecto compartido."
-  const descripcionKotlin = "aplicación móvil comparadora de textos en Kotlin, ofreciendo una herramienta eficiente para comparar y analizar contenido textual de manera precisa."
-  const descripcionFileTransfer = "Aplicación de transferencia de archivos entre procesos concurrentemente utilizando sockets UDP con protocolos Stop & Wait y Selective Repeat. Se realizó en Python. Proyecto compartido."
-  const descripcionfrontPSA = "Frontend sobre gestión de tickets realizando request a una API. Se utiliza Typescript; Next.js y React. Para el diseño se empleó Bootstrap"
-  const descripcionApiRest = "Backend utilizando Python y SQLAlchemy para la manipulación eficiente de datos, respaldado por una API REST para la gestión de información."
-  
-  const linkRocketLeague = "https://github.com/NahuelNGomez/Rocket-League"
-  const linkTeg = "https://github.com/NahuelNGomez/TEG"
-  const linkKotlin = "https://github.com/NahuelNGomez/Proyecto-final-Comparador"
-  const linkfrontPSA = "https://github.com/NahuelNGomez/Frontend-PSA"
-  const linkApiRest = "https://github.com/NahuelNGomez/soporteAPI"
-  const linkFileTransfer = "https://github.com/franciscoduc4/TP1-File-Transfer"
   return (
     <main className="min-h-screen">
       <Navegation/>
       <About/>
+
       <div id="work" className="grid sm:grid-cols-2 grid-cols-1 gap-4 px-24 ">
-        <div className="col-span-1 mb-10">
-          <TableWorks image={rocketLeague} title={titleRocketLeague} description={descripcionRocketLeague} link={linkRocketLeague} />
-        </div>
-        <div className="col-span-1 mb-10">
-          <TableWorks image={teg} title={titleTeg} description={descripcionTeg} link={linkTeg}/>
-        </div>
-        <div className="col-span-1 mb-10">
-          <TableWorks image={frontPSA} title={titlefrontPSA} description={descripcionfrontPSA} link={linkfrontPSA}/>
-        </div>
-        <div className="col-span-1 mb-10">
-          <TableWorks image={apiRest} title={titleApiRest} description={descripcionApiRest} link={linkApiRest}/>
-        </div>
-        <div className="col-span-1  mb-10">
-          <TableWorks image={kotlin} title={titleKotlin} description={descripcionKotlin} link={linkKotlin}/>
-        </div>
-        <div className="col-span-1  mb-10">
-          <TableWorks image={fileTransfer} title={titleFileTransfer} description={descripcionFileTransfer} link={linkFileTransfer} />
-        </div>
+        {PROJECTS.map((project, index) => {
+          return (
+            <div key={index} className="col-span-1 mb-10">
+              <TableWorks image={project.image} title={project.title} description={project.description} link={project.link} tags={project.tags} />
+            </div>
+          )
+        })}
       </div>
       <div id="contact">
         <ContactMe />
